f1bacfc1e0264db108ef0b8237655ca91c2bd3f0
Nick <deckards2505@gmail.com>
2020-09-08 16:28:59 +0600

    Init repo

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function () {
+
+    var slider = new Swiper('.slider__container', {
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.swiper-custom-next',
+            prevEl: '.swiper-custom-prev',
+        },
+        breakpoints: {
+            0: {
+                centeredSlides: false,
+                slidesPerView: 1,
+                autoplay: {
+                    delay: 2000,
+                },
+            },
+            767: {
+                loop: true,
+                centeredSlides: true,
+                slidesPerView: 3,
+                navigation: {
+        },
+            }
+        }
+    });
+    
+
+});
